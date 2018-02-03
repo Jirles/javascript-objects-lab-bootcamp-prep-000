2e8b9434808e0e51afa3760ce3b8696b659a0208
Emily Jirles <emily.jirles@gmail.com>
2018-02-03 16:36:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,10 @@ function destructivelyUpdateObjectWithKeyAndValue (obj, key, value){
   return obj 
 }
 
+function deleteFromObjectByKey (obj, key){
+  var newObj = Object.assign({}, obj)
+  delete newObj[key]
+  return newObj
+}
 
 

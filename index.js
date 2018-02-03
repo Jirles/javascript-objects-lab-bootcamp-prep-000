8761873b68f98a509b3bc0d52efa8070f9ade63d
Emily Jirles <emily.jirles@gmail.com>
2018-02-03 16:35:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var recipes = {eggs: 3, butter:'1/2 cup'}
 
 function updateObjectWithKeyAndValue (obj, key, value){
   var updatedObject = Object.assign({}, obj, {[key]: value})
-  return obj
+  return updatedObject
 }
+
+function destructivelyUpdateObjectWithKeyAndValue (obj, key, value){
+  obj[key] = value
+  return obj 
+}
+
+
 
